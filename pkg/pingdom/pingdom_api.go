061541c8e647019f1bf762fde768b776b7b9874c
Ross Fairbanks <ross@rossfairbanks.com>
2017-02-08 10:32:39 +0100

    Call Pingdom API to create and delete HTTP checks.

---
+++ pkg/pingdom/pingdom_api.go
@@ -0,0 +1,31 @@
+package pingdom
+
+import (
+	pdom "github.com/russellcardullo/go-pingdom/pingdom"
+)
+
+const (
+	checkInterval = 1
+)
+
+// Creates a HTTP check for the host and returns the Pingdom ID.
+func (c *Operator) createCheck(host string) (int, error) {
+	hc := pdom.HttpCheck{Name: host, Hostname: host, Resolution: checkInterval}
+	check, err := c.pclient.Checks.Create(&hc)
+	if err != nil {
+		log.Errorf("Failed to create check for host %s: %v", host, err)
+		return -1, err
+	}
+
+	return check.ID, err
+}
+
+// Deletes the HTTP check.
+func (c *Operator) deleteCheck(checkID int) error {
+	_, err := c.pclient.Checks.Delete(checkID)
+	if err != nil {
+		log.Errorf("Failed to delete check %d: %v", checkID, err)
+	}
+
+	return err
+}
